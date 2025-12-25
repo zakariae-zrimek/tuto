3524886d295bc12ac764b50fc426fc084aa856f0
Zakariae <zakariaezrimek95@gmail.com>
2025-12-25 14:44:38 +0100

    modification

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Counter } from './service/counter';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('tuto');
+
+  constructor(public counterService: Counter) {}
+
 }
